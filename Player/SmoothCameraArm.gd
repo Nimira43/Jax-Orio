c628bbc20625c00ab9968eeac2a4d90fe4850546
Nimira43 <n.rabone72@aol.com>
2025-02-26 02:01:40 +0000

    Using interpolation, the camera movement will be smoother and more gradual as it follows the target node.

--- Player/SmoothCameraArm.gd
+++ Player/SmoothCameraArm.gd
@@ -1,3 +1,6 @@
 extends SpringArm3D
 
 @export var target: Node3D
+
+func _physics_process(delta: float) -> void:
+	global_transform = global_transform.interpolate_with(target.global_transform, 0.5)
