a2917d599e2b08253a1ca3365333623ff05a2e76
Nimira43 <n.rabone72@aol.com>
2025-06-11 09:57:48 +0100

    Created update_animation_tree.

--- Player/rig.gd
+++ Player/rig.gd
@@ -2,3 +2,9 @@ extends Node3D
 
 @onready var animation_tree: AnimationTree = $AnimationTree
 var run_path: String = "parameters/MoveSpace/blend_position"
+
+func update_animation_tree(direction: Vector3) -> void:
+	if direction.is_zero_approx():
+		animation_tree[run_path] = -1.0
+	else:
+		animation_tree[run_path] = 1.0
