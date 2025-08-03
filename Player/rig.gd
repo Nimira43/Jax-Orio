0310c0e8a5a4ca3c2a2feb639777751d96d71ed1
Nimira43 <n.rabone72@aol.com>
2025-08-03 09:08:59 +0100

    Creating new run_weight_target variable

--- Player/rig.gd
+++ Player/rig.gd
@@ -2,6 +2,7 @@ extends Node3D
 
 @onready var animation_tree: AnimationTree = $AnimationTree
 var run_path: String = "parameters/MoveSpace/blend_position"
+var run_weight_target := -1.0
 
 func update_animation_tree(direction: Vector3) -> void:
 	if direction.is_zero_approx():
