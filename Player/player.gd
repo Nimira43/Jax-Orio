2b08fd08cae1ea79e1d0dbaa1fbc5c05a62d8313
Nimira43 <n.rabone72@aol.com>
2025-08-10 01:41:57 +0100

    slash_attack function

--- Player/player.gd
+++ Player/player.gd
@@ -46,6 +46,9 @@ func _unhandled_input(event: InputEvent) -> void:
 	if Input.mouse_mode == Input.MOUSE_MODE_CAPTURED:
 		if event is InputEventMouseMotion:
 			_look = -event.relative * mouse_sensitivity
+	if rig.is_idle():
+		if event.is_action_pressed("click"):
+			slash_attack()
 
 func get_movement_direction() -> Vector3:
 	var input_dir := Input.get_vector("move_left", "move_right", "move_forward", "move_back")
@@ -72,3 +75,6 @@ func look_toward_direction(direction: Vector3, delta: float) -> void:
 		target_transform,
 		1.0 - exp(-animation_decay * delta)
 	)
+	
+func slash_attack() -> void:
+	rig.travel("Slash")
